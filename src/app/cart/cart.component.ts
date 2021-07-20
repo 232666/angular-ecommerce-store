5f75f575bd4fed4274bfc84e583122bfe866bfa0
232666 <billaresdiego@gmail.com>
2021-07-19 21:21:57 -0300

    Aggregated of form functionality in the products checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -20,7 +20,6 @@ export class CartComponent {
   ) {}
 
   onSubmit(): void {
-    // Process checkout data here
     this.items = this.cartService.clearCart();
     console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
